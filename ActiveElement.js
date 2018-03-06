d3df17d4a2737d35fcdffcf0bb70398989487f19
Martin Domig <martin.domig@finkzeit.at>
2018-03-06 14:54:30 +0100

    added build files for deployment test

---
+++ ActiveElement.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ActiveElement = /** @class */ (function () {
+    function ActiveElement() {
+    }
+    return ActiveElement;
+}());
+exports.ActiveElement = ActiveElement;
+exports.default = ActiveElement;
